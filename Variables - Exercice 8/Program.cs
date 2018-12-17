985c55e2369468ea0455dbdd326ff45a4a6bfa1a
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-17 15:38:51 +0100

    Exercice completé

---
+++ Variables - Exercice 8/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables___Exercice_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rayon;
+            double perimetre;
+
+            Console.WriteLine("Quel est le rayon du cercle ? (r)");
+            rayon = int.Parse(Console.ReadLine());
+            perimetre = 2 * Math.PI * rayon;
+
+            Console.WriteLine("Le périmètre du cercles est de " + perimetre + " cm");
+        }
+    }
+}
